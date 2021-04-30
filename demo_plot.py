00f52bbc1fd3d76791c4ebd5c4ab6228cf40ed80
Raj Kumar Rana <rgk.rana@gmail.com>
2021-05-01 01:30:04 +0200

    Fixed bug with i_world

--- demo_plot.py
+++ demo_plot.py
@@ -9,6 +9,7 @@ n_waypoints = 22  # start + 20 inner points + end
 n_dim = 2
 n_paths_per_world = 1000
 n_worlds = 5000
+path_rows = [0, 1, 2, 1000, 2000]
 
 def main(db_path):
 	worlds = get_values_sql(file=db_path, table='worlds')
@@ -18,7 +19,7 @@ def main(db_path):
 	# 0...999     -> world 0
 	# 1000...1999 -> world 1
 	# 2000...2999 -> world 2
-	paths = get_values_sql(file=db_path, table='paths', rows=[0, 1, 2, 1000, 2000])
+	paths = get_values_sql(file=db_path, table='paths', rows=path_rows)
 	path_images = compressed2img(img_cmp=paths.path_img_cmp.values, n_voxels=n_voxels, n_dim=n_dim)
 	start_images = compressed2img(img_cmp=paths.start_img_cmp.values, n_voxels=n_voxels, n_dim=n_dim)
 	end_images = compressed2img(img_cmp=paths.end_img_cmp.values, n_voxels=n_voxels, n_dim=n_dim)
@@ -26,9 +27,9 @@ def main(db_path):
 	q_paths = object2numeric_array(paths.q_path.values)
 	q_paths = q_paths.reshape(-1, n_waypoints, n_dim)
 
-	# Plot and example
+	# Plot an example
 	i = 0
-	i_world = paths.i_world[i].item()
+	i_world = paths.i_world[path_rows[i]].item()
 
 	import matplotlib.pyplot as plt
 	fig, ax = plt.subplots()
@@ -45,5 +46,5 @@ def main(db_path):
 if __name__ == "__main__":
 	# database_path = Path of SingleSphere02.db database file
 	database_path = sys.argv[1]
-	assert database_path.endswith(".db"), f"Invalid database file detected! Received '{database_path}'"
+	assert database_path.endswith(".db"), f"Invalid database file path detected! Received '{database_path}'"
 	main(database_path)
