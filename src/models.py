b8ae9a47f63977e20da8ea66ec96cd11293ac83c
Raj Kumar Rana <rgk.rana@gmail.com>
2021-05-11 12:08:38 +0200

    Minor changes

--- src/models.py
+++ src/models.py
@@ -1,16 +1,16 @@
 from tensorflow.compat.v1.keras import Model
 from tensorflow.compat.v1.keras.layers import Input, Conv2D, Concatenate
 
-from layers import DenseBlock, TransitionBlock
+from .layers import DenseBlock, TransitionBlock
 
 
-def u_dense_net(input_shape, num_db, num_channels=64, growth_rate=32, convs_per_db=3, **kwargs):
+def u_dense_net(input_shape, num_db, num_channels=64, growth_rate=32, convs_per_db=3):
     assert len(input_shape) == 3, f"Input shape must have 3 dimension! Received '{input_shape}'!"
-    assert (input_shape[0] % 2 == 0) and (input_shape[1] % 2 == 0), f"Width and height of the image must be a even number! Received '{input_shape[:2]}'!"
     assert (num_db > 1) and (num_db % 2 == 1), f"Number of DenseBlocks must be an odd number more than 1! Received '{num_db}'!"
-    assert (num_channels > 0) and (num_channels % 2 == 0), f"Number of channels for TransitionBlock must be an even number more than 0! Received '{num_channels}'!"    
     # In a U-shaped DenseNet with N DenseBlocks, each side has floor(N/2) DenseBlocks  
     num_trans_down = num_trans_up = num_db//2
+    assert (input_shape[0] % 2**num_trans_down == 0) and (input_shape[1] % 2**num_trans_down == 0), f"Dimension of the input shape {input_shape[:2]} must be a multiple of {2**num_trans_down} to preserve the tensor shape after down-scaling and up-scaling"
+    assert (num_channels > 0) and (num_channels % 2 == 0), f"Number of channels for TransitionBlock must be an even number more than 0! Received '{num_channels}'!"    
     img_in = Input(dtype="float32", shape=input_shape, name="image_input")
     x = Conv2D(growth_rate, kernel_size=(5, 5), activation="relu", padding="same")(img_in)
     ############################### Transition down section ###############################
