13059b2f4d14829c8beb7c7e05094af98f8140e8
Raj Kumar Rana <rgk.rana@gmail.com>
2021-05-11 12:07:25 +0200

    Removed default callback

--- src/generators.py
+++ src/generators.py
@@ -2,24 +2,20 @@ import numpy as np
 from tensorflow.compat.v1.keras.utils import Sequence
 
 class DataGen(Sequence):
-    def __init__(self, data_dict, batch_size=32, callback=None):
+    def __init__(self, data_dict, callback, batch_size=32, length_key="path_rows"):
         self.data_dict = data_dict
         self.batch_size = batch_size
-        num_items = len(self.data_dict["goal_imgs"])
+        num_items = len(self.data_dict[length_key])
         self.indexes = np.arange(num_items, dtype="uint16")
         self.gen_len = np.math.ceil(num_items/self.batch_size)
-        self.callback = self.default_callback if callback is None else callback
+        self.callback = callback
         assert callable(self.callback)
     
     def __len__(self):
         return self.gen_len
     
-    @staticmethod
-    def default_callback(batch_indexes, data_dict):
-        return {key: val[batch_indexes] for key, val in data_dict.items()}
-    
     def __getitem__(self, index):
-        lower_index = index * self.batch_size
+        lower_index = index*self.batch_size
         higher_index = lower_index + self.batch_size
         batch_indexes = self.indexes[lower_index:higher_index]
         batch_data = self.callback(batch_indexes, self.data_dict)
