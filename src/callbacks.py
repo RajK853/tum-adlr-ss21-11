1e055124f0f4683888367eb3edb2e09ecccd9977
Raj Kumar Rana <rgk.rana@gmail.com>
2021-06-15 10:42:15 +0200

    Added support for tf.data.Dataset

--- src/callbacks.py
+++ src/callbacks.py
@@ -8,21 +8,27 @@ Callback = tf.keras.callbacks.Callback
 
 
 class ImageSaverCallback(Callback):
-    def __init__(self, data_gen, log_dir, callback):
+    __slots__ = ("data_gen", "data_iter", "log_dir", "callback", "pbar", "index")
+    
+    def __init__(self, data_gen, log_dir, callback, data_gen_size=100):
         self.data_gen = data_gen
+        self.data_iter = iter(data_gen)
         self.log_dir = log_dir
         self.callback = callback
-        os.makedirs(log_dir, exist_ok=True)
-        N = len(self.data_gen.indexes)
-        self.pbar = ProgressBar(total_iter=N, title=f"Saving {N} images:")
+        self.pbar = ProgressBar(total_iter=data_gen_size, title=f"Saving images:")
         self.index = 0
+        os.makedirs(self.log_dir, exist_ok=True)
+    
+    def on_predict_begin(self, logs=None):
+        self.index = 0
+        self.data_iter = iter(self.data_gen)
     
     def on_predict_batch_end(self, batch, logs=None):
-        batch_size = logs["outputs"].shape[0]
-        batch_inputs, batch_outputs = self.data_gen[batch]
-        logs["inputs"] = batch_inputs[0]
-        logs["true_outputs"] = batch_outputs[0]
+        batch_inputs, batch_outputs = next(self.data_iter)
+        logs["inputs"] = batch_inputs.numpy()
+        logs["true_outputs"] = batch_outputs.numpy()
         self.callback(self.index, logs, self.log_dir)
+        batch_size = logs["outputs"].shape[0]
         self.pbar.step(batch_size)
         self.index += batch_size
         
