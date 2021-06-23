fb0cc3a7adfe4682dd4a8b88e290b4d0e685c7e0
Raj Kumar Rana <rgk.rana@gmail.com>
2021-06-23 23:38:55 +0200

    Removed DataGen

--- train_image2image_model.py
+++ train_image2image_model.py
@@ -7,7 +7,6 @@ import tensorflow.compat.v1 as tf
 
 from src import losses
 from src.models import u_dense_net
-from src.generators import DataGen
 from src.utils import exec_from_yaml, dump_yaml
 from src.callbacks import ImageSaverCallback
 from src.load import get_values_sql, compressed2img, object2numeric_array
